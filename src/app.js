d2e18ab8594a9cbc9e5b0dd6860b6066623c78aa
Joan Wilson <joanwmoliveira@gmail.com>
2022-01-20 17:18:40 -0300

    Estruturando express

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+
+
+class App {
+    constructor() {
+        this.server = express();
+    }
+
+    middlewares() {
+
+    }
+
+    routes() {
+
+    }
+}
+
+export default new App().server;
