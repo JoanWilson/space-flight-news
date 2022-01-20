113ca9ef2194de6c1ace533899397be595a16920
Joan Wilson <joanwmoliveira@gmail.com>
2022-01-20 17:22:14 -0300

    Instalando cors

--- src/app.js
+++ src/app.js
@@ -5,14 +5,19 @@ import cors from 'cors';
 class App {
     constructor() {
         this.server = express();
+        this.middlewares();
+        this.routes();
     }
 
     middlewares() {
-
+        this.server.use(express.json());
+        this.server.use(cors());
     }
 
     routes() {
-
+        this.server.get('/', (req, res) => {
+            res.send('Conectado a porta 5000');
+        })
     }
 }
 
